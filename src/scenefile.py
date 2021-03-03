65c0dc44f9862b0f49fb8ecae107c0c6e8cd3343
Saw170000 <79948213+Saw170000@users.noreply.github.com>
2021-03-02 19:17:46 -0600

    Initializes from a Path

--- src/scenefile.py
+++ src/scenefile.py
@@ -1,14 +1,18 @@
-from pathlib import Path  # Makes Paths into a Path object
+from pathlib import Path
 
 
 class SceneFile(object):
     """An abstract representation of a Scene file."""
-    def __init__(self, folder_path, descriptor, task, ver, ext):
-        self.folder_path = Path(folder_path)   # now a 'Path' object
-        self.descriptor = descriptor
-        self.task = task
-        self.ver = ver
-        self.ext = ext
+
+    def __init__(self, path):
+        self.folder_path = Path()
+
+        # Default values in case they're missing from the path
+        self.descriptor = 'main'
+        self.task = None
+        self.ver = 1
+        self.ext = '.ma'
+        self._init_from_path(path)  # A private method starts with '_'
 
     @property
     def filename(self):
@@ -18,11 +22,18 @@ class SceneFile(object):
                               ver=self.ver,
                               ext=self.ext)
 
-    # Now will use proper slashes in path name
     @property
     def path(self):
         return self.folder_path / self.filename
 
+    def _init_from_path(self, path):
+        path = Path(path)
+        self.folder_path = path.parent
+        self.ext = path.suffix
+        self.descriptor, self.task, ver = path.stem.split("_")
+        self.ver = int(ver.split("v")[-1])
 
-scene_file = SceneFile("D:/", "tank", "model", 1, ".ma")
+
+scene_file = SceneFile("D:/tank_model_v001.ma")
 print(scene_file.path)  # prints D:\tank_model_v001.ma
+print(scene_file.filename)  # prints tank_model_v001.ma
