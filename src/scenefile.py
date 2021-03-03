2553d405147ce5182c3077f56ea9a21819eef5a6
Saw170000 <79948213+Saw170000@users.noreply.github.com>
2021-03-02 18:56:36 -0600

    Update scenefile.py

--- src/scenefile.py
+++ src/scenefile.py
@@ -7,6 +7,24 @@ class SceneFile(object):
         self.ver = ver
         self.ext = ext
 
+    # Added @property with the 'filename' composite
+    # ver now has up to 3 digits
+    @property
+    def filename(self):
+        pattern = "{descriptor}_{task}_v{ver:03d}{ext}"
+        return pattern.format(descriptor=self.descriptor,
+                              task=self.task,
+                              ver=self.ver,
+                              ext=self.ext)
 
-scene_file = SceneFile("D:\\", "tank", "model", "v001", ".ma")
-print(scene_file.descriptor)
+    # Added @property with the 'path' composite
+    @property
+    def path(self):
+        result = self.folder_path + "/" + self.filename
+        return result
+
+
+# now prints path
+scene_file = SceneFile("D:\\", "tank", "model", 1, ".ma")
+print(scene_file.filename)
+print(scene_file.path)
